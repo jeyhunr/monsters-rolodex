d007350f1eed588e9d32cdac0cd799554c008dd7
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2022-05-18 19:49:10 +0200

    how react component works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import CardList from './components/component-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      seaerchField: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          }
+        );
+      });
+  }
+
+  onSearchChange = (event) => {
+    const seaerchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { seaerchField };
+    });
+  }
+
+  render() {
+    const { monsters, seaerchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(seaerchField);
+    });
+
+    return (
+      <div className="App">
+        <h1 className="app-title">Monster Rolodex</h1>
+        <SearchBox onSearchHandler={onSearchChange} class="search-monster" placeholder="search monster" />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
